0bfd1ebdbfd4e0a1e4e48f1ee9cb5b65a480322b
Duc <ducgamedev@gmail.com>
2026-06-25 09:21:26 +0700

    feat: redesign UI — light theme, Space Grotesk/Manrope/JetBrains Mono, hero homepage, full article layout

--- posts/views.py
+++ posts/views.py
@@ -7,10 +7,22 @@ class PostListView(ListView):
     model = Post
     template_name = 'posts/index.html'
     context_object_name = 'posts'
-    paginate_by = 10
+    paginate_by = 6
+
+    def _published(self):
+        return Post.objects.filter(status=Post.STATUS_PUBLISHED).select_related('author', 'category')
 
     def get_queryset(self):
-        return Post.objects.filter(status=Post.STATUS_PUBLISHED).select_related('author', 'category')
+        qs = self._published()
+        featured = qs.first()
+        if featured:
+            return qs.exclude(pk=featured.pk)
+        return qs
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['featured'] = self._published().first()
+        return ctx
 
 
 class PostDetailView(DetailView):
@@ -26,7 +38,7 @@ class PostDetailView(DetailView):
         ctx['related_posts'] = Post.objects.filter(
             status=Post.STATUS_PUBLISHED,
             category=self.object.category,
-        ).exclude(pk=self.object.pk)[:3]
+        ).exclude(pk=self.object.pk).select_related('author', 'category')[:3]
         return ctx
 
 
@@ -34,7 +46,7 @@ class CategoryDetailView(ListView):
     model = Post
     template_name = 'posts/category.html'
     context_object_name = 'posts'
-    paginate_by = 10
+    paginate_by = 6
 
     def get_queryset(self):
         self.category = get_object_or_404(Category, slug=self.kwargs['slug'])
